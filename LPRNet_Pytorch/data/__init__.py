1814fc98d1163f5a88b4a6b9f836581fa5bc697c
Wentao@Xiong2geely.com <xwt1079944650@gmail.com>
2023-10-07 09:20:18 +0800

    first commit

---
+++ LPRNet_Pytorch/data/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+# @Author: fanstuck
+# @Time: 2023/9/25 10:42
+# @File: __init__.py
+from .load_data import *
---
+++ LPRNet_Pytorch/model/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+# @Author: fanstuck
+# @Time: 2023/9/25 10:43
+# @File: __init__.py
+from .LPRNet import *
