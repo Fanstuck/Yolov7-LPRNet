13f962fb37309ffa0c27f9c888877e7986960177
Wentao@Xiong2geely.com <xwt1079944650@gmail.com>
2023-10-09 11:42:59 +0800

    v1.0

--- LPRNet_Pytorch/test.py
+++ LPRNet_Pytorch/test.py
@@ -22,6 +22,7 @@ import torch.nn as nn
 import numpy as np
 import argparse
 import torch
+import pandas as pd
 import time
 import cv2
 import os
@@ -29,15 +30,15 @@ import os
 def get_parser():
     parser = argparse.ArgumentParser(description='parameters to train net')
     parser.add_argument('--img_size', default=[94, 24], help='the image size')
-    parser.add_argument('--test_img_dirs', default=r"D:\python_Project\LPRnet_ccpd\LPRNet_Pytorch-master\data\test_try", help='the test images path')
+    parser.add_argument('--test_img_dirs', default="./data/test", help='the test images path')
     parser.add_argument('--dropout_rate', default=0, help='dropout rate.')
     parser.add_argument('--lpr_max_len', default=8, help='license plate number max length.')
-    parser.add_argument('--test_batch_size', default=100, help='testing batch size.')
+    parser.add_argument('--test_batch_size', default=10, help='testing batch size.')
     parser.add_argument('--phase_train', default=False, type=bool, help='train or test phase flag.')
     parser.add_argument('--num_workers', default=4, type=int, help='Number of workers used in dataloading')
     parser.add_argument('--cuda', default=True, type=bool, help='Use cuda to train model')
     parser.add_argument('--show', default=True, type=bool, help='show test image and its predict result or not.')
-    parser.add_argument('--pretrained_model', default='./weights/LPRNet__iteration_2000.pth', help='pretrained base model')
+    parser.add_argument('--pretrained_model', default='./weights/Final_LPRNet_model.pth', help='pretrained base model')
 
     args = parser.parse_args()
 
@@ -159,14 +160,17 @@ def show(img, label, target):
     flag = "F"
     if lb == tg:
         flag = "T"
+
     #img = cv2.putText(img, lb, (0,16), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.6, (0, 0, 255), 1)
     img = cv2ImgAddText(img, lb, (0, 0))
-    #cv2.imshow("test", img)
+    cv2.imshow("test", img)
     print("target: ", tg, " ### {} ### ".format(flag), "predict: ", lb)
+    traget_file.append(tg)
+    predict_data.append(lb)
     cv2.waitKey()
     cv2.destroyAllWindows()
 
-def cv2ImgAddText(img, text, pos, textColor=(255, 0, 0), textSize=12):
+def cv2ImgAddText(img, text, pos, textColor=(255, 0, 0), textSize=20):
     if (isinstance(img, np.ndarray)):  # detect opencv format or not
         img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
     draw = ImageDraw.Draw(img)
@@ -177,4 +181,11 @@ def cv2ImgAddText(img, text, pos, textColor=(255, 0, 0), textSize=12):
 
 
 if __name__ == "__main__":
+    traget_file=[]
+    predict_data=[]
     test()
+    dict_convert = {"target_license":traget_file,"predict_license": predict_data}
+    print(len(traget_file))
+    print(len(predict_data))
+    df_convert = pd.DataFrame(dict_convert)
+    df_convert.to_csv('./predict_data.csv',encoding='utf-8')
